1f97b72885cd848f5992003d6b0fbfb9ec559686
the11977 <56327222+the11977@users.noreply.github.com>
2019-12-03 19:10:24 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -5,44 +5,56 @@ import os
 # Module for reading CSV files
 import csv
 
-csvpath = os.path.join('Resources', 'election_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
-total_votes = 0
-candidate_options = []
-candidate_votes = {}
-winning_candidate = ""
-winning_count = 0
+#Improved Reading using CSV module
 
-with open(csvpath, newline='') as election_data:
+
+total_month = 0
+total_net = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+
+with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
-    # csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-    reader = csv.reader(election_data)
-    # Read the header
-    header = next(reader)
+    print(csvreader)
 
-#row 2 because index starts with 0
-    for row in reader: 
-        total_votes = total_votes + 1
-        candidate_name = row[2]
-        if candidate_name not in candidate_options:
-            candidate_options.append(candidate_name)
-            candidate_votes[candidate_name] = 0
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-#Data mine the winner 
-    for candidate in candidate_votes:
-        votes = candidate_votes.get(candidate)
-        vote_percentage = float(votes) / float(total_votes) * 100
-        
-#Determine which candidate is winner
-        if (votes > winning_count):
-            winning_count = votes
-            winning_candidate = candidate
-        print (candidate, votes, vote_percentage)
-    print (total_votes)
-    print (winning_candidate)
-    print (winning_count)
-    
+    # Read the header row first (skip this step if there is now header)
+    header = next(csvreader)
+    #print(f"CSV Header: {csvheader}")
 
+    first_row = next(csvreader)
+    total_month = total_month + 1
+    total_net = total_net + int(first_row[1])
+    previous_net = int(first_row[1])
 
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+        total_month = total_month + 1
+        total_net = total_net + int(row[1])
+ # Track the net change
+        net_change = int(row[1]) - previous_net
+        previous_net = int(row[1])
+        net_change_list = net_change_list + [net_change]
+        month_of_change = month_of_change + [row[0]]
+        # Calculate the greatest increase
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+        # Calculate the greatest decrease
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+    net_monthly_average = sum(net_change_list)/len(net_change_list)
+
+    print (total_month)
+    print (total_net)
+    print (net_monthly_average)
+    print (greatest_increase[0], greatest_increase[1])
+    print (greatest_decrease[0], greatest_decrease[1])
